02e248e184101318e168287520540142f11bee67
Sangameswaran <sangamezhil@gmail.com>
2025-07-27 10:00:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 'use client';
 
 import { useEffect } from 'react';
-import type { Metadata } from 'next';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
 import { useCompanyProfile } from '@/lib/data';
@@ -31,6 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>LoanTrack Lite</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
